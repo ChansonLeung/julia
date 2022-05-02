51fc06442383c000278caab7e0b8cbbf40bb470b
Chanson <liangchangsong@163.com>
2022-05-03 03:35:47 +0800

    九一分

--- problem_5_predict.jl
+++ problem_5_predict.jl
@@ -6,6 +6,8 @@ using Plots
 import Base
 plotlyjs()
 
+roundup(num) = round(num, RoundNearestTiesUp)
+
 #状态变量 
 ##P 需求量 
 P = begin
@@ -35,25 +37,32 @@ train = zeros(size(P))
 function back_buy(i, diff_old)
     # 今天得多买diff_old个充当后面的老手
     # 1. 当前老手足够多买diff_old个
-    # 今天能用-今天用-今天计划老手{ceil((原本买 + 再多买)*师生比)}
-    if remain[i] - 4 * P[i] - ceil((X[i] + diff_old) * c) >= 0
+    # 全部参与培训，够用
+    if floor((remain[i] - 4 * P[i])/c) >= X[i]+diff_old
+    # if remain[i] - 4 * P[i] - ceil((X[i] + diff_old) * c) >= 0
         X[i] += diff_old
         remain[i+1] += diff_old
-        return
     else
         # 2. 当前老手不够多买diff_old个
-        #原则：能买几个算几个,剩下的再往前面买
-        if diff_old != 1
-            # 假如按原本计划下，剩下0.15个能用作老手,则还可以再买floor(0.15/c)
-            okbuy = floor((remain[i] - 4 * P[i] - X[i] * c) / c)
-            X[i] += okbuy
-            remain[i+1] += diff_old
-            back_buy(i - 1, diff_old - okbuy)
-        else
-            remain[i+1] += diff_old
-            back_buy(i - 1, diff_old)
+        #原则：和主循环一样先能买多少买多少，剩下一九分
+        markX = X[i]
+        X[i] = floor((remain[i] - 4 * P[i] ) / c) 
+        # 还差的less，分成两部分，一部分今天买X[i]，另一部分交给前面
+        less = markX + diff_old - X[i]
+        if less == 1
+            back_buy(i - 1, 1)
+        else 
+            # 优先保证往前买的老手是整数
+            X[i] += less-ceil(less*c) 
+            back_buy(i - 1, ceil(less*c))
         end
+        remain[i+1] += diff_old
     end
+    # 今天保养(后面要买的话再减) = 今天能用 - 今天用 + 昨天用 - 昨天坏
+    in_rest[i] = remain[i] - P[i] + P[i-1] - roundup(τ*P[i-1]) - ceil(X[i]*c)
+    # 当天训练量
+    train[i] = X[i] + ceil(X[i]*c)
+    print("hi")
 end
 
 
@@ -62,9 +71,9 @@ for i = 3:length(P)-2
     @label start_make_buying_plan
     # 今天不买的情况下
     # 明天能用 = 今天能用 - 今天用  + 昨天用 - 昨天坏 
-    remain_p1 = remain[i] - 4 * P[i] + 4 * P[i-1] - ceil(τ * 4 * P[i-1])
-    # 今天保养(后面要买的话再减) = 今天能用 - 今天用 + 昨天用 - 昨天坏
-    in_rest[i] = remain[i] - P[i] + P[i-1] - round(τ*P[i-1])
+    remain_p1 = remain[i] - 4 * P[i] + 4 * P[i-1] - roundup(τ * 4 * P[i-1])
+    # 今天保养(后面要买的话再减) = 明天能用的
+    in_rest[i] = remain_p1 
     # 不买明天也够
     if remain_p1 >= 4 * P[i+1]
         #则不买
@@ -73,7 +82,8 @@ for i = 3:length(P)-2
     else
         # 不够,今天得买刚好够明天的量diff
         diff = 4 * P[i+1] - remain_p1
-        # 1. 老手够用->培训量(今天能用-今天用)/师生比  足够培训今天想买的新手
+        # 1. 老手够用->培训量:{剩余老手:{今天能用-今天用}/师生比}  足够培训今天想买的新手
+        @show remain[i], 4*P[i]
         if floor((remain[i] - 4 * P[i]) / c) >= diff
             #直接买
             X[i] = diff
@@ -81,20 +91,30 @@ for i = 3:length(P)-2
             remain[i+1] = 4 * P[i+1]
         else
         # 2. 老手不够用,回头找机会买老手
-            # 差的老手的量 = 没有老师的新生/师生比 |> 向上取整
-            diff_old = (diff - floor((remain[i] - 4 * P[i]) / c)) * c |> ceil
-            back_buy(i - 1, diff_old)
+            # 差的老手的量O+买的量X = diff
+            # 剩下老师傅都参与培训,能买X[i]
+            X[i] = floor((remain[i] - 4 * P[i]) / c) 
+            # 还差的less，分成两部分，一部分今天买X[i]，另一部分交给前面
+            less =diff - X[i]
+            if less == 1
+                back_buy(i - 1, 1)
+            else 
+                # 优先保证往前买的老手是整数
+                X[i] += less-ceil(less*c) 
+                back_buy(i - 1, ceil(less*c))
+            end
             # 修改了前面的购买情况，使得老手凑齐或明天够用, 重新这轮计划
             @goto start_make_buying_plan
         end
     end
     # 减去用来培训的
     in_rest[i] -= ceil(X[i]*c)
-    # 当天训练
-    train[i] = X[i] + X[i]*c
+    # 当天训练量
+    train[i] = X[i] + ceil(X[i]*c)
 end
 
-cost = 100*sum(X[2:end-2]) + 5*sum(in_rest[2:end-2])
+cost = 100*sum(X[2:end-2]) + 5*sum(in_rest[2:end-2]) + 10*sum(X[2:end-2]+ ceil.(c*X[2:end-2]) )
+plot(X)
 
 sum(X)
 sum(P*0.1*4)
